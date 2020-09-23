5f3a38528556fc8b21e30cca66370cbf46550f6e
Sanduni-Wijerathne <55197806+Sanduni-Wijerathne@users.noreply.github.com>
2020-09-23 17:46:56 +0530

    add admin files

---
+++ src/java/Alogin.java
@@ -0,0 +1,111 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import beans.Alogin_bean;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Oops
+ */
+public class Alogin extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        try (PrintWriter out = response.getWriter()) {
+           
+            
+           Alogin_bean alb = new Alogin_bean(); 
+           alb.setEmail(request.getParameter("email"));
+          
+           alb.setPassword(request.getParameter("pass"));
+           
+           
+           MyDB db = new MyDB();
+           Connection con =db.getCon();
+           Statement stmt = con.createStatement();
+          
+           ResultSet rs = stmt.executeQuery("select name,email,password from admins where email = '"+alb.getEmail()+"' and password = '"+alb.getPassword()+"'"); 
+           if(rs.next()) {
+           String n = rs.getString("email");
+           HttpSession session=request.getSession();  
+           session.setAttribute("mail",n);
+           session.setAttribute("role","admin");
+           response.sendRedirect("adminDash.jsp");
+//            out.println(alb.getEmail());
+           
+           }else{
+                response.sendRedirect("ErrorA.html");
+           }
+        } catch (SQLException ex) {
+            Logger.getLogger(login.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        }
+    
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/java/beans/Alogin_bean.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package beans;
+
+/**
+ *
+ * @author kavee
+ */
+public class Alogin_bean  implements java.io.Serializable{  
+    private String email;
+    private String password;
+    
+    public Alogin_bean(){}
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
